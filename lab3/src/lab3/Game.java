d8b9f813e25eb299a03636329ea6645f7f71e2d9
kittisuk007 <showl3oy@hotmail.com>
2017-02-06 15:06:58 +0700

    ส่งงาน lab3 6/2/60

---
+++ lab3/src/lab3/Game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author BoYnT^
+ */
+public abstract class Game  {
+    public void initialize(){
+    }
+    public void startPlay(){
+    }
+    public void endPlay(){
+    }
+    public void play(){
+    }
+}
